423ae93abb117380805324614c63af6718a3c0e3
binaryjep <jeffescueta13@gmail.com>
2024-05-02 23:48:17 +0800

    NOTHING IMPORTANT

--- asp.py
+++ asp.py
@@ -8,6 +8,7 @@ def activity_selection(start:list, finish:list):
     selected_activities = [activities[0]]  # Select the first activity
 
     for i in range(1, n):  # Iterate through the remaining activities
+        
         if activities[i][1] >= selected_activities[-1][0]:
             selected_activities.append(activities[i])
 
@@ -31,7 +32,7 @@ if __name__ == '__main__':
 
     print("\n\033[93mSelected Activities:\033[0m") 
 
-    for activity in selected: 
-        print("\033[92mStart: {}\033[0m, \033[91mFinish: {}\033[0m".format(activity[1], activity[0]))  
+    for i, activity in enumerate(selected): 
+        print("Activity {} \033[92mStart: {}\033[0m, \033[91mFinish: {}\033[0m".format(i +1,activity[1], activity[0]))  
 
     print(f"\nThe number of activities selected are \033[93m{len(selected)}\033[0m")
